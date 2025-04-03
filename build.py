98feac88165d0f3961c2a22e30958a47c39fc586
twonfi <127453854+twonfi@users.noreply.github.com>
2025-04-03 16:08:33 -0700

    auto build script for CLI

---
+++ build.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Easy build script for manually building from /.
+
+Do not use for automated processes or shortcut buttons.
+ This is only for the CLI.
+"""
+
+import os
+os.chdir('elSSGin')
+from elSSGin import __main__
+
