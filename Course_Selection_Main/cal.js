cec7485290e4e3c6971616cad40048ec29b44f59
twonfi <git@twonum.org>
2025-12-01 16:09:45 -0800

    Set `cal.js` to use internal proxy

--- Course_Selection_Main/cal.js
+++ Course_Selection_Main/cal.js
@@ -11,15 +11,7 @@ window.addEventListener("load", function () {
         events: async function (fetchInfo, successCallback, failureCallback) {
           try {
             const response = await fetch(
-                // DO NOT USE IN PRODUCTION I'LL THINK OF BETTER WAY
-                // (but it would probably be used anyways) -> Yep lmao will use
-
-                /* note:
-                 we can use /orca_news/calendar.json if Appazur blocks the proxy's IP or if the proxy blocks us
-                 it's not dynamic (script needs to be run manually) but I have a home server that can run this,
-                 or we can use gh actions (free minutes); it takes a few seconds to run so not a concern
-                */
-              "https://api.codetabs.com/v1/proxy?quest=elginpark.appazur.com/api/a?age=30&ua=1&v=2"
+              "/orca-news/data?api=a"
             );
             const data = await response.json();
 
