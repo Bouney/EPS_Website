c7131687d50b2cb179a0c1887a6dad7192310a76
twonfi <git@twonum.org>
2025-12-01 20:03:52 -0800

    chore: Refactor

--- Course_Selection_Main/Countdown.js
+++ Course_Selection_Main/Countdown.js
@@ -1,23 +1,20 @@
 // used in one.js
 export default function Countdown(time, element) {
-    var x = setInterval(function() {
+    const x = setInterval(function() {
         // Get today's date and time
-        var now = new Date().getTime();
+        const now = new Date().getTime();
 
         // Find the distance between now and the count down date
-        var distance = time - now;
+        const distance = time - now;
 
         // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor(
-            (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-        );
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        console.log(time);
-        // Display the result in the element with id="demo"
-        element.innerHTML =
-            days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result
+        element.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
         // If the count down is finished, write some text
         if (distance < 0) {
